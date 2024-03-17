4b7c3ada33cdcf8e04dbab528cf369def9ec7595
ItsMadruga <npkaiorr@gmai.com>
2024-03-17 15:48:12 -0300

    separei o ocultar gravando/digitando

--- app/src/main/java/its/madruga/wpp/xposed/plugins/privacy/XGhostMode.java
+++ app/src/main/java/its/madruga/wpp/xposed/plugins/privacy/XGhostMode.java
@@ -1,12 +1,12 @@
 package its.madruga.wpp.xposed.plugins.privacy;
 
+import static its.madruga.wpp.ClassesReference.GhostMode.methodName;
 import static its.madruga.wpp.ClassesReference.GhostMode.param1;
 import static its.madruga.wpp.ClassesReference.GhostMode.param2;
 
-import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedHelpers;
-import its.madruga.wpp.ClassesReference;
 import its.madruga.wpp.xposed.models.XHookBase;
 
 public class XGhostMode extends XHookBase {
@@ -20,13 +20,23 @@ public class XGhostMode extends XHookBase {
         Class<?> class1 = XposedHelpers.findClass(param1, loader);
         Class<?> class2 = XposedHelpers.findClass(param2, loader);
 
-        var ghostmode = prefs.getBoolean("ghostmode", false);
-        if (!ghostmode) return;
+        var ghostmode_t = prefs.getBoolean("ghostmode_t", false);
+        var ghostmode_r = prefs.getBoolean("ghostmode_r", false);
 
-        XposedHelpers.findAndHookMethod(class1, ClassesReference.GhostMode.methodName, class1, class2, int.class, new XC_MethodReplacement() {
+
+        XposedHelpers.findAndHookMethod(class1, methodName, class1, class2, int.class, new XC_MethodHook() {
             @Override
-            protected Object replaceHookedMethod(MethodHookParam param) {
-                return null;
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                var p1 = (int) param.args[2];
+                if (p1 == 1 && ghostmode_r) {
+                    param.setResult(null);
+                    return;
+                }
+                if (p1 == 0 && ghostmode_t) {
+                    param.setResult(null);
+                    return;
+                }
+                super.beforeHookedMethod(param);
             }
         });
     }
